78f70646de5188b18e904100dc8ae705c8403f6e
olle sandahl <olle.sandahl@live.se>
2020-02-26 18:46:13 +0100

    Forms

--- Presentationslager/Program.cs
+++ Presentationslager/Program.cs
@@ -16,7 +16,7 @@ namespace Presentationslager
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoggInForm());
         }
     }
 }
