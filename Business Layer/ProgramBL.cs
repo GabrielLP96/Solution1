584c97d0fcc13b7149dd594610f7fa6f602afd9c
Gabriel Lundberg Puglia <s174099@student.hb.se>
2020-02-27 20:06:51 +0100

    hej

---
+++ Business Layer/ProgramBL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Layer
+{
+    public class ProgramBL : Data_Layer.Program
+    { 
+        public static ProgramBL Convert (Data_Layer.Program program)
+        {
+            return new ProgramBL()
+            {
+                Name = program.Name,
+            };
+
+        }
+    }
+}
